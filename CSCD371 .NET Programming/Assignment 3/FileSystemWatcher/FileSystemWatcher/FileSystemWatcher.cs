9db5e6a73fa5b6e5c25f194c083179b16eb020d7
Ethan Tuning <etuning5@hotmail.com>
2017-10-11 22:07:05 -0700

    Add files via upload

--- CSCD371 .NET Programming/Assignment 3/FileSystemWatcher/FileSystemWatcher/FileSystemWatcher.cs
+++ CSCD371 .NET Programming/Assignment 3/FileSystemWatcher/FileSystemWatcher/FileSystemWatcher.cs
@@ -1,44 +1,113 @@
 ﻿using System;
 using System.IO;
-using System.Security.Permissions;
-using FileSystemWatcher.SQL;
 
 namespace FileSystemWatcher {
+
     class FileSystemWatcher {
 
-        private static string watchFile;
+        private static string mWatchDir;
         private static System.IO.FileSystemWatcher FSWatcher;
-        private static SqlDatabaseLogger log;
+        private static Logger log;
+        private static bool mIsDir;
 
         static void Main(string[] args) {
-
+            log = new Logger();
+            Run();
         }
 
-        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
         public static void Run() {
 
             string filePath = getFilePath();
 
             FSWatcher = new System.IO.FileSystemWatcher();
             FSWatcher.Path = filePath;
-            FSWatcher.NotifyFilter = NotifyFilters.CreationTime | 
-                                     NotifyFilters.LastAccess | 
-                                     NotifyFilters.LastWrite | 
-                                     NotifyFilters.DirectoryName | 
+            FSWatcher.NotifyFilter = NotifyFilters.CreationTime |
+                                     NotifyFilters.LastAccess |
+                                     NotifyFilters.LastWrite |
+                                     NotifyFilters.DirectoryName |
                                      NotifyFilters.FileName;
 
-            FSWatcher.Changed += FileSystemEventHandler(onCreated);
-            FSWatcher.Changed += FileSystemEventHandler(onDeleted);
-            FSWatcher.Changed += FileSystemEventHandler(onChanged);
-            FSWatcher.Changed += RenamedEventHandler(onRemaned);
+            if(mIsDir) {
+                FSWatcher.Filter = "";
+                FSWatcher.IncludeSubdirectories = true;
+            }
+            else {
+                FSWatcher.Filter = mWatchDir;
+            }
+
+            FSWatcher.Created += new FileSystemEventHandler(onCreated);
+            FSWatcher.Deleted += new FileSystemEventHandler(onDeleted);
+            FSWatcher.Changed += new FileSystemEventHandler(onChanged);
+            FSWatcher.Renamed += new RenamedEventHandler(onRenamed);
 
             FSWatcher.EnableRaisingEvents = true;
+
+            log.SetWatch(mWatchDir);
+
             Console.WriteLine("Press 'e' to exit.");
-            while (Console.Read() != 'e');
+            while (Console.Read() != 'e') ;
+        }
+
+        private static void onCreated(object obj, FileSystemEventArgs e) {
+
+            string path = e.FullPath;
+            DateTime time = DateTime.Now;
+            string file = new FileInfo(path).Name;
+            string eventType = "File Created";
+            Console.WriteLine("File: {0} {1} created on {2}", file, path, time.ToString());
+            log.Log(file, path, eventType, time.ToString());
+        }
+
+        private static void onDeleted(object obj, FileSystemEventArgs e) {
+
+            string path = e.FullPath;
+            DateTime time = DateTime.Now;
+            string file = new FileInfo(path).Name;
+            string eventType = "File Deleted";
+            Console.WriteLine("File: {0} {1} was deleted on {2}", file, path, time.ToString());
+            log.Log(file, path, eventType, time.ToString());
+        }
+
+        private static void onChanged(object obj, FileSystemEventArgs e) {
+            string path = e.FullPath;
+            DateTime time = DateTime.Now;
+            string file = new FileInfo(path).Name;
+            string eventType = "File Changed";
+            Console.WriteLine("File: {0} {1} was changed on {2}", file, path, time.ToString());
+            log.Log(file, path, eventType, time.ToString());
+        }
+
+        private static void onRenamed(object obj, RenamedEventArgs e) {
+            string path = e.FullPath;
+            DateTime time = DateTime.Now;
+            string file = new FileInfo(path).Name;
+            string eventType = "File Renamed";
+            Console.WriteLine("File: {0} {1} was renamed on {2}", file, path, time.ToString());
+            log.Log(file, path, eventType, time.ToString());
         }
 
         private static string getFilePath() {
-            return "";
+
+            string file = "";
+            bool exists = false;
+
+            while(exists == false) {
+                Console.Write("Enter file to watch: ");
+                file = Console.ReadLine();
+
+                if (Directory.Exists(file)) {
+                    exists = true;
+                    mIsDir = true;
+                }
+                else if (File.Exists(file)) {
+                    exists = true;
+                    mIsDir = false;
+                }
+                else {
+                    exists = false;
+                }
+            }
+            return file;
         }
     }
 }
---
+++ CSCD371 .NET Programming/Assignment 3/FileSystemWatcher/FileSystemWatcher/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+public class Logger {
+
+    private StreamWriter mStream;
+
+	public Logger() {
+        mStream = File.AppendText("log.txt");
+	}
+
+    public void Log(string file, string path, string eventType, string time) {
+        mStream.WriteLine("File: {0} {1} was "+ eventType +" on {2}", file, path, time.ToString());
+        mStream.Flush();
+    }
+
+    public void SetWatch(String watchDir) {
+        mStream.WriteLine(watchDir);
+        mStream.Flush();
+    }
+}
